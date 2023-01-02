b8664081c6d8146df4f0ba78810078b6c432c6a5
Morgan Frost <morganfrostse@gmail.com>
2023-01-01 23:56:29 -0600

    CRUD and DbContext created

--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -17,6 +17,20 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    app.UseDeveloperExceptionPage();
+    //app.UseMigrationsEndPoint();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<SchoolContext>();
+    context.Database.EnsureCreated();
+    // DbInitializer.Initialize(context);
+}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
